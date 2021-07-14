5f176ab8688e5ac858f84f0c6986d073e355a665
creamcheesesteak <imth19921@gmail.com>
2021-07-14 09:40:15 +0900

    config 파일삭제

--- home/views.py
+++ home/views.py
@@ -1,6 +1,25 @@
+from urllib import request
+
 from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse('<h1>안녕하세요. 임태혁입니다</h1>')
+    path = request.path
+    resultstr = ''
+    if path == '/home':
+        resultstr = '<h1>여기는 home입니다</h1>'
+    else :
+        resultstr = '<h1>여기는 main입니다</h1>'
+
+
+    return HttpResponse(resultstr)
+
+def index01(ruquest):
+    result = {'first':'taehyeok', 'second':'im'}
+    return render(request, 'index.html', context=result)
+
+def index01(ruquest):
+    request.GET['']
+    result = {'first':request.GET['taehyeok'], 'second':request.GET['im']}
+    return render(request, 'index.html', context=result)
